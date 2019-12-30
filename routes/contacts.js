6f886aaedf61e884a99a2a681ac127022fc2b22d
ikechukwu edwin <eo@hellofresh.com>
2019-12-30 13:51:33 +0100

    Setup routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../middleware/auth')
 
 // @route 	GET api/contacts
 // @access 	Private
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
 	res.send('Gets all contacts');
 })
 
