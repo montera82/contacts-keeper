68f871292b869186d9c0287a127ad900bc4d86c9
ikechukwu edwin <eo@hellofresh.com>
2019-12-31 17:45:29 +0100

    Add contactForm

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,52 @@
+import React, {useContext} from 'react';
+import PropTypes from 'prop-types';
+import ContactContext from "../../context/contact/contactContext";
+
+const ContactItem = ({contact}) => {
+	const {name, id, email, phone, type} = contact;
+	const contactContext = useContext(ContactContext);
+	const {deleteContact, setCurrent, clearCurrent} = contactContext;
+
+	const onDelete = () => {
+		deleteContact(id);
+		clearCurrent();
+	}
+	return (
+		<div className='card bg-light'>
+			<h3 className='text-primary text-left'>
+				{name} {' '}
+				<span style={{float: 'right'}}
+					  className={'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>
+
+				{type.charAt(0).toUpperCase() + type.slice(1)}
+
+			</span>
+			</h3>
+			<ul className='list'>
+				{email && (
+					<li>
+						<i className='fas fa-envelope-open'/> {email}
+					</li>
+				)}
+				{phone && (
+					<li>
+						<i className='fas fa-phone'/> {phone}
+					</li>
+				)}
+			</ul>
+			<p>
+				<button onClick={() => setCurrent(contact)}
+						className='btn btn-dark btn-sm'>
+					Edit
+				</button>
+				<button onClick={onDelete} className='btn btn-danger btn-sm'>
+					Delete
+				</button>
+			</p>
+		</div>
+	);
+};
+
+ContactItem.propTypes = {};
+
+export default ContactItem;
