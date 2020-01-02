6bbf6ead96825009242492da6bd76f5909cf752c
ikechukwu edwin <eo@hellofresh.com>
2020-01-02 18:29:31 +0100

    Modify navbar to display auth and private routes.

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, {Fragment, useContext} from 'react';
 import PropTypes from 'prop-types';
 import {Link} from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
 
 const Navbar = ({title, icon}) => {
+
+	const authContext = useContext(AuthContext);
+	const {logout, isAuthenticated, user} = authContext;
+
+	const onLogout = () => {
+		logout()
+	}
+
+	const authLinks = (
+		<Fragment>
+			<li>Hello {user && user.name},</li>
+			<li>
+				<a href="#!" onClick={onLogout}>
+					<i className="fas fa-sign-out-alt"/> <span className="hide-sm">Logout</span>
+				</a>
+			</li>
+			<li>
+				<Link to='/'>Home</Link>
+				<Link to='/about'>About</Link>
+			</li>
+
+		</Fragment>
+	);
+
+	const guestLinks = (
+		<Fragment>
+
+			<Link to='/register'>Register</Link>
+			<Link to='/login'>Login</Link>
+		</Fragment>
+	)
 	return (
 		<div className="navbar bg-primary">
 			<h1>
 				<i className={icon}/>{title}
 			</h1>
 			<ul>
-				<li>
-					<Link to='/'>Home</Link>
-					<Link to='/about'>About</Link>
-					<Link to='/register'>Register</Link>
-					<Link to='/login'>Login</Link>
-				</li>
+				{isAuthenticated ? authLinks : guestLinks}
 			</ul>
 		</div>
 	);
